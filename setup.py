a20079d68b943c48d793aafa4af6b2e80c26f6eb
LiamDoult <3579535@myuwc.ac.za>
2020-03-17 19:29:58 +0000

    Bump Version 0.3.0 -> 0.5.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='integration_tester',
-    version='0.3.0',
+    version='0.5.0',
     description='A Docker based service manager for testing in Python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
